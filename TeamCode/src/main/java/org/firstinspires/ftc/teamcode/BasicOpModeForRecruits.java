4bbd849bb5e38921af3daedb5ff2009fe7d9a5d3
bagae <141751300+bagae@users.noreply.github.com>
2025-08-29 11:21:17 +0300

    first_push_again

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpModeForRecruits.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+// TODO : Dacă ai nevoie poți importa clase sau librării suplimentare
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.values;
+
+@TeleOp(name="cum_vreti_sa_apara_pe_driver_station", group="Linear OpMode")
+public class BasicOpModeForRecruits extends LinearOpMode {
+
+    // timer
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // Declararea motoarelor
+    //Ex:
+    private DcMotor frontLeftDrive = null;
+    //Declararea servomotoarelor
+    //Ex:
+    private Servo mgn=null;
+
+    @Override
+    public void runOpMode() {
+
+        // TODO: Inițializează toate motoarele și servo-urile folosind hardwareMap
+        // Ex: frontLeftDrive  = hardwareMap.get(DcMotor.class, "nume_din_configuratie");
+
+        // Setează direcția motoarelor pentru ca robotul să meargă înainte când împingi stick-ul înainte
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Status inițial
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Așteaptă apăsarea butonului START
+        waitForStart();
+        runtime.reset();
+
+        // Rulează până la STOP
+        while (opModeIsActive()) {
+
+            // Citește valorile de la joystick
+            double drive  = -gamepad1.left_stick_y;  // față/spate
+            double strafe =  gamepad1.left_stick_x;  // stânga/dreapta
+            double turn   =  gamepad1.right_stick_x; // rotație
+
+            // Calculează puterea pentru fiecare motor (mecanum)
+            double frontLeftPower  = drive + strafe + turn;
+            double backLeftPower   = drive - strafe + turn;
+            double frontRightPower = drive - strafe - turn;
+            double backRightPower  = drive + strafe - turn;
+
+            // TODO: Normalizează valorile de putere să nu depășească [-1, 1]
+            double max = "TODO";
+
+            frontLeftPower  /= max;
+            backLeftPower   /= max;
+            frontRightPower /= max;
+            backRightPower  /= max;
+
+            // TODO: Trimite puterea calculată la motoare
+            // Ex: frontLeftDrive.setPower(frontLeftPower);
+
+
+
+            //TODO: Alegeti-vă trei servomotoare și programați-le
+
+            // TODO: Adaugă mesaje pe telemetry (pentru debugging)
+            // Ex: telemetry.addData("Timp rulare", "%.1f", runtime.seconds());
+            telemetry.update();
+        }
+    }
+}
+// TODO: Retine ca daca vrei poti folosi si clasa values pentru ca codul sa para mai frumos scris si mai putin incarcat
+//pentru a folosi valorile declarate in values ,vei folosi values.nume_valoare
+
+// TODO: Dacă reușești să termini tot, poți face o clasă RobotMap
+// unde declari și inițializezi toate motoarele și servo-urile (comentariile în română)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/values.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class values {
+   // TODO: Declara constantele pe care le vei folosi
+    //public static int a;
+    // F Important,declara variabilele publice pentru a le putea folosi si in alte clase!!!!!!
+}
