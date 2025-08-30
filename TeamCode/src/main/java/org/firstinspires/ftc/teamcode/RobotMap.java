8591957671f614e7c65f63b297da099989384748
bagae <141751300+bagae@users.noreply.github.com>
2025-08-30 16:30:36 +0300

    task_luni_reviewed_again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMap.java
@@ -16,15 +16,10 @@ public class RobotMap {
 
 
     public RobotMap(HardwareMap Init) {
-        /**
-         * Chassis
-         */
+
         //TODO: In metoda RobotMap vei accesa motoarele si servomotoarele(OBS:Accesarea se face utilizand parametrul Init)
         //Ex:frontRightDrive = Init.get(DcMotor.class, "rightFront");
 
-        /**
-         * PROPERTIES
-         */
         //TODO: Noi folosim pe robotul nostru motoarele de pe sasiu fără encoder.Vei avea de setat motoarele să meargă fără encoder.
         //Ex:frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
